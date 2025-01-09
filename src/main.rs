fc6a81d9ad0d3df1baff8ed30bb986e9cb20276a
Liam PRADENE <lpradene@made-f0Ar5s6.clusters.42paris.fr>
2025-01-09 18:20:07 +0100

    Linear combination

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ where K:
     std::ops::Add<Output = K> +
     std::ops::Sub<Output = K> + 
     std::ops::Mul<Output = K> + 
-    Copy,
+    Copy +
+    Default,
 {
     fn new(data: Vec<K>) -> Self {
         Vector {
@@ -97,16 +98,20 @@ where K:
         }
     }
 
-    fn mul_mat(&mut self, matrix: &Matrix<K>) {
+    fn mul_mat(&self, matrix: &Matrix<K>) -> Matrix<K> {
         assert_eq!(self.cols, matrix.rows, "Cannot multiply matrices cols and rows differ: {} vs {}", self.cols, matrix.rows);
+
+        let mut result = vec![vec![K::default(); matrix.cols]; self.rows];
 
         for i in 0..self.rows {
             for j in 0..matrix.cols {
                 for k in 0..self.cols {
-                    self.data[i][j] = self.data[i][j] + self.data[i][k] * matrix.data[k][j];
+                    result[i][j] = result[i][j] + self.data[i][k] * matrix.data[k][j];
                 }
             }
         }
+
+        Matrix::new(result)
     }
 
     fn mul_vec(&mut self, vector: &Vector<K>) -> Vector<K> {
@@ -146,6 +151,34 @@ where K:
     }
 }
 
+fn linear_combination<K>(vectors: &[Vector<K>], scalars: &[K]) -> Vector<K>
+where K:
+    std::ops::Add<Output = K> +
+    std::ops::Sub<Output = K> +
+    std::ops::Mul<Output = K> +
+    Clone +
+    Copy +
+    Default,
+{
+    // Check vectors length is not equal to 0
+    assert!(!vectors.is_empty(), "Vectors is empty");
+
+    // Check if vectors length and scalars length are equal
+    assert_eq!(vectors.len(), scalars.len(), "Vectors length and scalars length must be equal");
+    
+    // Check all vector of vectors have the same length
+    assert!(vectors.iter().all(|v| v.data.len() == vectors[0].data.len()), "All vectors must have the same length");
+
+    let mut result = vec![K::default(); vectors[0].data.len()];
+
+    for (scalar, vector) in scalars.iter().zip(vectors.iter()) {
+        result.iter_mut()
+            .zip(vector.data.iter())
+            .for_each(|(res, &v)| *res = *res + scalar.clone() * v.clone());
+    }
+
+    Vector::new(result)
+}
 
 fn main() {
     let mut matrix = Matrix::new(vec![
@@ -167,4 +200,9 @@ fn main() {
 
     let transposed = matrix.transpose();
     transposed.print();
+
+    let v1 = Vector::new(vec![1., 2., 3.]);
+    let v2 = Vector::new(vec![0., 10., -100.]);
+
+    println!("{:#?}", linear_combination(&[v1, v2], &[10., -2.]));
 }
