d976d49121d75db4603e5711a0f031ad349f9e30
Pradene <liam.pradene@gmail.com>
2025-01-28 19:59:25 +0100

    Adding radian

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub use lerp::lerp;
+pub use radian::radian;
 pub use vector::{Vector, linear_combination};
 pub use matrix::Matrix;
 
 pub mod matrix;
 pub mod vector;
 pub mod lerp;
+pub mod radian;
---
+++ src/radian.rs
@@ -0,0 +1,3 @@
+pub fn radian(angle: f32) -> f32 {
+    return angle * (std::f64::consts::PI as f32 / 180.);
+}
