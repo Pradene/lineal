234f279ef4b67fa8ea92014a81297885c78c502f
Pradene <liam.pradene@gmail.com>
2025-11-23 13:14:07 +0100

    .

--- src/lerp.rs
+++ src/lerp.rs
@@ -1,9 +1,6 @@
 use std::ops::{Add, Mul};
 
-pub fn lerp<T>(x: T, y: T, t: f32) -> T
-where
-    T: Add<Output = T> + Mul<f32, Output = T>,
-{
+pub fn lerp<T: Add<Output = T> + Mul<f32, Output = T>>(x: T, y: T, t: f32) -> T {
     let t = t.clamp(0., 1.);
     x * (1. - t) + y * t
 }
