7877c8f640b4121816f6b067aea4202f1a2b5119
Pradene <liam.pradene@gmail.com>
2025-11-23 13:31:02 +0100

    Remove useless radian method

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 pub mod lerp;
 pub mod matrix;
 pub mod number;
-pub mod radian;
 pub mod vector;
 
 pub use matrix::Matrix;
 pub use vector::Vector;
 
 pub use lerp::lerp;
-pub use radian::radian;
--- src/radian.rs
+++
@@ -1,8 +0,0 @@
-use std::{
-    f32::consts::PI,
-    ops::{Add, Mul},
-};
-
-pub fn radian<T: Add<Output = T> + Mul<f32, Output = T>>(angle: T) -> T {
-    angle * (PI / 180.0)
-}
