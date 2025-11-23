5151a6ceaa789761ba1276779154c0ab629efc64
Pradene <liam.pradene@gmail.com>
2025-11-23 13:18:29 +0100

    .

--- src/radian.rs
+++ src/radian.rs
@@ -1,3 +1,8 @@
-pub fn radian(angle: f64) -> f64 {
-    angle * (std::f64::consts::PI / 180.0)
+use std::{
+    f32::consts::PI,
+    ops::{Add, Mul},
+};
+
+pub fn radian<T: Add<Output = T> + Mul<f32, Output = T>>(angle: T) -> T {
+    angle * (PI / 180.0)
 }
